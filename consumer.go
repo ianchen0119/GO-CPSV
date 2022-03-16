c43fc839941502aea8464fddcb78eed51467f5ca
Ian Chen <ychen.desl@gmail.com>
2022-03-16 01:29:08 +0000

    add consumer.go

---
+++ consumer.go
@@ -0,0 +1,28 @@
+package main
+
+import "C"
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/ianchen0119/GO-CPSV/cpsv"
+)
+
+type Vertex struct {
+	X int32
+	Y int32
+}
+
+func main() {
+	cpsv.Start()
+	len := cpsv.GetSize(Vertex{})
+
+	fmt.Scanln()
+
+	readData := cpsv.Load("d1", 0, len)
+	var bufV *Vertex = *(**Vertex)(unsafe.Pointer(&readData))
+	fmt.Printf("X: %d, Y:%d\n", bufV.X, bufV.Y)
+
+	fmt.Scanln()
+	cpsv.Destroy()
+}
