f40bac082bb1bc9e86dd7088a86aec3c84a4ed0a
Ian Chen <ychen.desl@gmail.com>
2022-03-17 15:23:00 +0800

    update Copyright on cpsv.c

--- cpsv/cpsv.c
+++ cpsv/cpsv.c
@@ -1,3 +1,29 @@
+/*      -*- OpenSAF  -*-
+ *
+ * (C) Copyright 2008 The OpenSAF Foundation
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. This file and program are licensed
+ * under the GNU Lesser General Public License Version 2.1, February 1999.
+ * The complete license can be accessed from the following location:
+ * http://opensource.org/licenses/lgpl-license.php
+ * See the Copying file included with the OpenSAF distribution for full
+ * licensing terms.
+ *
+ * Original Author(s): Emerson Network Power
+ */
+
+/*****************************************************************************
+..............................................................................
+MODULE NAME: cpsv.c (derived from cpsv_test_app.c)
+
+  .............................................................................
+  DESCRIPTION:
+
+  This program interacts with OpenSAF CKPT and GO-CPSV.
+******************************************************************************/
+
 #include "go-cpsv.h"
 
 SaCkptHandleT ckptHandle;
