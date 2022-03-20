d5ea3ef8cb0103f1989d5b123bc37ae9c50f836e
Ian Chen <ychen.desl@gmail.com>
2022-03-20 07:07:06 +0000

    add json example

--- json_example.go
+++ json_example.go
@@ -3,7 +3,8 @@ package main
 import "C"
 import (
 	"fmt"
-	"unsafe"
+	// "unsafe"
+	"encoding/json"
 
 	"github.com/ianchen0119/GO-CPSV/cpsv"
 )
@@ -17,17 +18,21 @@ type Location struct {
 func main() {
 	cpsv.Start("safCkpt=TEST1,safApp=safCkptService")
 	loc := &Location{X: 25, Y: 23, Z: 50}
-	len := cpsv.GetSize(Location{})
-	wbuf := cpsv.GoBytes(unsafe.Pointer(loc), len)
+	jsonLoc, _ := json.Marshal(loc)
 
-	cpsv.NonFixedStore("d1", wbuf, int(len))
+	len := len(jsonLoc)
+	fmt.Println(jsonLoc)
+
+	cpsv.NonFixedStore("d1", jsonLoc, int(len))
 
 	fmt.Scanln()
 
 	readData, err := cpsv.NonFixedLoad("d1")
 
 	if err == nil {
-		var bufL *Location = *(**Location)(unsafe.Pointer(&readData))
+		fmt.Println(readData)
+		var bufL Location
+		json.Unmarshal(readData, &bufL)
 		fmt.Printf("X: %d, Y:%d, Z: %d\n", bufL.X, bufL.Y, bufL.Z)
 	} else {
 		fmt.Println("got errors:", err)
